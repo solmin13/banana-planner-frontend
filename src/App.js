74f38a8f67d6adcaa6362ed08a772b7977f21b89
solmin13 <133374141+solmin13@users.noreply.github.com>
2024-01-11 04:19:40 +0900

    todoList 개발중

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './css/App.css';
 import { useEffect, useState } from 'react';
 import {Routes, Route, Link} from 'react-router-dom'
 import axios from 'axios';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <TodoItem></TodoItem>
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+function TodoList(props){
+    return(
+        <div>
+            <div>
+                <ul>
+                    {/* {props.todoData.map(()=>{})} */}
+                </ul>
+            </div>
+            <button id='addTodoBtn'>+</button>
+        </div>
+    );
+}
+export default TodoList;
